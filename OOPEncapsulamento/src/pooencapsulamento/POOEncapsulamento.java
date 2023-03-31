1390ab341b4c384958a46641354eea0647554f5f
Bruno Vargas <brunocalegaridevargas@gmail.com>
2023-03-31 18:46:42 -0300

    added oop encapsulation project

---
+++ OOPEncapsulamento/src/pooencapsulamento/POOEncapsulamento.java
@@ -0,0 +1,16 @@
+
+package pooencapsulamento;
+
+public class POOEncapsulamento {
+    public static void main(String[] args) {
+        
+        ControleRemoto c1 = new ControleRemoto();
+        c1.desligar();
+        c1.maisVolume();
+        c1.play();
+        c1.abrirMenu();
+        c1.fecharMenu();
+   
+    }
+    
+}
