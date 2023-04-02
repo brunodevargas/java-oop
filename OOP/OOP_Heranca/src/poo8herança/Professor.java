93087448abb557ad86394e35b3e758a8cdc1a2b0
Bruno Vargas <brunocalegaridevargas@gmail.com>
2023-04-02 17:03:58 -0300

    oop book

---
+++ OOP/OOP_Heranca/src/poo8herança/Professor.java
@@ -0,0 +1,30 @@
+package poo8herança;
+
+public class Professor extends Pessoa {
+    
+    private String especialidade;
+    private double salario;
+    
+    public void receberAumento(double aum) {
+        this.salario += aum;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    
+    
+}
---
+++ OOP/OOP_Heranca_Sobreposicao/src/poo9herançasobreposição/Visitante.java
@@ -0,0 +1,9 @@
+package poo9herançasobreposição;
+public class Visitante extends Pessoa {
+    /* VISITANTE TEM HERANÇA DE IMPLEMENTAÇÃO (HERANÇA "POBRE")
+    NÃO TEM NENHUM ATRIBUTO/MÉTODO PROPRIO, APENAS OS HERDADO PELA CLASSE PESSOA (MÃE)
+           
+*/
+    
+    
+}
